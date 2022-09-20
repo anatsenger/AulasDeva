0af2bfb7a443a6ea277a77bb61b63b4fad1826d8
anatsenger <anathaissenger@hotmail.com>
2022-09-19 22:41:59 -0300

    correções

--- src/ex1.java
+++ src/ex1.java
@@ -4,11 +4,8 @@ public class ex1 {
         double velocidadeMedia = 80.0;
         double distacia = tempoGasto * velocidadeMedia;
         double litrosUsados = distacia / 12;
-//        System.out.println("Para uma viagem de " + tempoGasto + " horas com a velocidade de " +velocidadeMedia+
-//                "km/h percorreu-se a distancia de " + distacia + "km e foram necessários " + litrosUsados +
-//                " litros de combustível");
-        System.out.printf("Para uma viagem de %.1f horas com a velocidade de %.2fKm/h percorreu-se a" +
-                " distancia de %.2fKm e foram necessários %.2fL de combustível.", tempoGasto, velocidadeMedia, distacia, litrosUsados);
+        System.out.printf("Para uma viagem de %.1f horas com a velocidade de %.2fKm/h, percorreu-se a" +
+                " distância de %.2fKm e foram necessários %.2fL de combustível.", tempoGasto, velocidadeMedia, distacia, litrosUsados);
     }
 
 }
--- src/ex4.java
+++ src/ex4.java
@@ -6,7 +6,7 @@ public class ex4 {
         double cotacaoDolar = 5.17;
         double valorReais = 20.0;
         double converteReais = valorReais * cotacaoDolar;
-        System.out.printf("A cotação do dolar hoje é R$:%.2f, portando R$:%.2f ficam US$:%.2f dolares", cotacaoDolar, valorReais, converteReais);
+        System.out.printf("A cotação do dolar hoje é de R$:%.2f, portanto R$:%.2f ficam US$:%.2f dolares", cotacaoDolar, valorReais, converteReais);
 
     }
 }
