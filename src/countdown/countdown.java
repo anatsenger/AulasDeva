dc63f8c5a6d16423306f3f0fc3940ce4920c53e5
anatsenger <anathaissenger@hotmail.com>
2022-09-28 21:04:45 -0300

    correções

--- src/countdown/countdown.java
+++ src/countdown/countdown.java
@@ -14,14 +14,14 @@ public class countdown {
     public static void main(String[] args) {
 
         LocalDateTime dataHoje = LocalDateTime.now();
-        LocalDateTime midnigth = LocalDateTime.of(2022, 10, 21, 00, 00, 00);
-        Duration duracao = Duration.between(dataHoje, midnigth);
+        LocalDateTime midnightTS10 = LocalDateTime.of(2022, 10, 21, 00, 00, 00);
+        Duration duracao = Duration.between(dataHoje, midnightTS10);
         System.out.println(duracao.toDaysPart() + " dias " + duracao.toHoursPart() + " horas e " + duracao.toMinutesPart() + " minutos");
         while (!duracao.equals("PT0S")){
             try{
                 dataHoje = LocalDateTime.now();
-                midnigth = LocalDateTime.of(2022, 10, 21, 00, 00, 00);
-                duracao = Duration.between(dataHoje, midnigth);
+                midnightTS10 = LocalDateTime.of(2022, 10, 21, 00, 00, 00);
+                duracao = Duration.between(dataHoje, midnightTS10);
                 Thread.sleep(1000*60);
                 System.out.println(duracao.toDaysPart() + " dias " + duracao.toHoursPart() + " horas e " + duracao.toMinutesPart() + " minutos");
             }catch (InterruptedException e){
